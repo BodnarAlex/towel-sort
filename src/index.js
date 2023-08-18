4ae08a1a30271b9d6287e890f8674f9e62fcaf58
Bodnar Alex <allugovskova@mail.ru>
2023-08-18 21:26:39 +0200

    refactor: improve code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   let res = [];
-  if(!(matrix)){
-    return res;
-  }
-
-  for(let i = 0; i < matrix.length; i++){
-    if(i % 2 === 0)
-      res = res.concat(matrix[i]);
-    else
-      res = res.concat((matrix[i].reverse()));
-  }
-  return res;
+  if(matrix)
+    res = matrix.map((x)=> (matrix.indexOf(x) % 2) ? x.reverse() : x);
+  return [].concat(...res);
 }
